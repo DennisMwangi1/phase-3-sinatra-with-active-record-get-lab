2af95b45d0b52b3a124b8a1d4932f5663c3e5d2d
DennisMwangi1 <dennis.kamau@student.moringaschool.com>
2022-08-30 07:22:23 +0300

    commit tree 9c8f9fae75275c8713c661fb93b598d2bb189771

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,11 @@ class ApplicationController < Sinatra::Base
     bakery.to_json(include: :baked_goods)
   end
 
+  get '/baked_goods' do
+    baked_goods=BakedGood.all
+    baked_goods.to_json
+  end
+
   get '/baked_goods/by_price' do
     baked_goods=BakedGood.all.order('price DESC')
     baked_goods.to_json
